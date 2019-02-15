310d5dc652afea217291cc7d9f69f42946d5b55e
StevenJBurns <stevenjburns@msn.com>
2019-02-15 01:17:19 -0700

    Start clean up in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import logo from './assets/logo.svg';
+import './App.scss';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Edit the <code>src/App.js</code> file and save to reload.</p>
           <a
             className="App-link"
             href="https://reactjs.org"
